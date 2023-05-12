d152dc36cdc563620add36edda4aaf6da7f22465
Gaurika-Kulshrestha <128786167+Gaurika-Kulshrestha@users.noreply.github.com>
2023-05-12 12:39:03 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -3,6 +3,7 @@ function addUser() {
     user_name = document.getElementById("user_name").value;
 
     localStorage.setItem("user_name", user_name);
-
+    firebaseConfig.database().ref("/").child(user_name).update({
+      purpose : "adding user" });
       window.location = "kwitter_room.html";
 }
